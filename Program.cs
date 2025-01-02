019d729dc9a93b608a98cc2239906eb43f6e9880
Otthon Leão <otthonleao@hotmail.com>
2025-01-02 01:45:46 -0400

    refactor: injection dependency config = services.AddScoped<>();

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.RegisterDatabase(builder.Configuration);
-builder.Services.AddScoped<IJobRepository, JobRepository>();
+builder.Services.RegisterRepositories();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
