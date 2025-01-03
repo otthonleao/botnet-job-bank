c9f77fd46f9f67f7b03a98daae35df8aac10e08a
Otthon Leão <otthonleao@hotmail.com>
2025-01-03 03:56:16 -0400

    chore: add Heteoas manually just to educational process

--- Api/Jobs/Controllers/JobController.cs
+++ Api/Jobs/Controllers/JobController.cs
@@ -1,3 +1,4 @@
+using JobBank.Api.Jobs.Common;
 using JobBank.Api.Jobs.Dtos;
 using JobBank.Api.Jobs.Services;
 using Microsoft.AspNetCore.Mvc;
@@ -18,27 +19,49 @@ public class JobController : ControllerBase
     [HttpGet]
     public IActionResult FindAll()
     {
-        return Ok(_jobService.FindAll());
+        // return Ok(_jobService.FindAll());
+        var body = _jobService.FindAll();
+        foreach (var resource in body)
+        {
+            var selfLink = new LinkResponseHetoas($"/api/jobs/{resource.Id}", "GET", "self");
+            var updateLink = new LinkResponseHetoas($"/api/jobs/{resource.Id}", "PUT", "update");
+            var deleteLink = new LinkResponseHetoas($"/api/jobs/{resource.Id}", "DELETE", "delete");
+            resource.AddLinks(selfLink, updateLink, deleteLink);
+        }
+        return Ok(body);
     }
     
     [HttpGet("{id}")]
     public IActionResult FindById([FromRoute] int id)
     {
-        return Ok(_jobService.FindById(id));
+        // return Ok(_jobService.FindById(id));
+        var body = _jobService.FindById(id);
+        body.AddLink(new LinkResponseHetoas($"/api/jobs/{body.Id}", "GET", "self"));
+        body.AddLink(new LinkResponseHetoas($"/api/jobs/{body.Id}", "PUT", "update"));
+        body.AddLink(new LinkResponseHetoas($"/api/jobs/{body.Id}", "DELETE", "delete"));
+        return Ok(body);
     }
     
     [HttpPost]
     public IActionResult Create([FromBody] JobRequest jobRequest)
     {
-        var body = _jobService.Create(jobRequest);
         // return Created($"/api/jobs/{body.Id}", _jobService.Create(jobRequest));
+        var body = _jobService.Create(jobRequest);
+        body.AddLink(new LinkResponseHetoas($"/api/jobs/{body.Id}", "GET", "self"));
+        body.AddLink(new LinkResponseHetoas($"/api/jobs/{body.Id}", "PUT", "update"));
+        body.AddLink(new LinkResponseHetoas($"/api/jobs/{body.Id}", "DELETE", "delete"));
         return CreatedAtAction(nameof(FindById), new { id = body.Id }, body);
     }
     
     [HttpPut("{id}")]
     public IActionResult Update([FromRoute] int id, [FromBody] JobRequest jobRequest)
     {
-        return Ok(_jobService.Update(id, jobRequest));
+        // return Ok(_jobService.Update(id, jobRequest));
+        var body = _jobService.Update(id, jobRequest);
+        body.AddLink(new LinkResponseHetoas($"/api/jobs/{body.Id}", "GET", "self"));
+        body.AddLink(new LinkResponseHetoas($"/api/jobs/{body.Id}", "PUT", "update"));
+        body.AddLink(new LinkResponseHetoas($"/api/jobs/{body.Id}", "DELETE", "delete"));
+        return Ok(body);
     }
     
     [HttpDelete("{id}")]
