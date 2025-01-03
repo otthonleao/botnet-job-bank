287d6b7a0837229a4fef19dc5dd04b9729ec6fa2
Otthon Leão <otthonleao@hotmail.com>
2025-01-02 20:46:56 -0400

    chore: add endpoint GET to Find All and By ID

---
+++ Core/Exceptions/ModelNotFoundException.cs
@@ -0,0 +1,8 @@
+namespace JobBank.Core.Exceptions;
+
+public class ModelNotFoundException : Exception
+{
+    public ModelNotFoundException(string? message = "Model not found") : base(message)
+    {
+    }
+}
