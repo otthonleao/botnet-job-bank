32406740a1cebb9f95c3aec70cce71edfe92db7a
Otthon Leão <otthonleao@hotmail.com>
2025-01-02 22:06:17 -0400

    refactor: change DatabaseConfig and compose file

--- Core/Config/DatabaseConfig.cs
+++ Core/Config/DatabaseConfig.cs
@@ -5,9 +5,10 @@ namespace JobBank.Core.Config;
 
 public static class DatabaseConfig
 {
-    public static void RegisterDatabase(this IServiceCollection services, ConfigurationManager builderConfiguration)
+    public static void RegisterDatabase(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddDbContext<JobBankDbContext>(options =>
-            options.UseNpgsql("Host=localhost;Port=5433;Database=dotnet-job-bank_db;Username=user;Password=senha123"));
+            options.UseNpgsql(configuration.GetConnectionString("Database"))
+                .LogTo(Console.WriteLine, LogLevel.Information));
     }
 }
