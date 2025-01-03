79bf4cdaa9ab7915376ae85b80ee4521914daf98
Otthon Leão <otthonleao@hotmail.com>
2025-01-03 06:57:37 -0400

    chore: attributes ignored to didn't show in Json Response

--- Api/Common/Dtos/PagedResponse.cs
+++ Api/Common/Dtos/PagedResponse.cs
@@ -1,3 +1,4 @@
+using System.Text.Json.Serialization;
 using JobBank.Api.Jobs.Common;
 
 namespace JobBank.Api.Jobs.Dtos;
@@ -5,12 +6,18 @@ namespace JobBank.Api.Jobs.Dtos;
 public class PagedResponse<R> : ResourceResponseHetoas
 {
     public ICollection<R> Items { get; set; } = new List<R>();
+    [JsonIgnore]
     public int PageNumber { get; set; }
+    [JsonIgnore]
     public int PageSize { get; set; }
+    [JsonIgnore]
     public int FirstPage { get; set; }
+    [JsonIgnore]
     public int LastPage { get; set; }
     public int TotalPages { get; set; }
     public int TotalElements { get; set; }
+    [JsonIgnore]
     public bool HasPreviousPage { get; set; }
+    [JsonIgnore]
     public bool HasNextPage { get; set; }
 }
