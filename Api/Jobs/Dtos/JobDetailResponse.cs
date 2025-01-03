00a7a720a4724e2b37d12b050356609c8882c6e4
Otthon Leão <otthonleao@hotmail.com>
2025-01-03 03:40:38 -0400

    chore: add base link hetoas structure and add in responses classes

--- Api/Jobs/Dtos/JobDetailResponse.cs
+++ Api/Jobs/Dtos/JobDetailResponse.cs
@@ -1,9 +1,12 @@
+using JobBank.Api.Jobs.Common;
+
 namespace JobBank.Api.Jobs.Dtos;
 
-public class JobDetailResponse
+public class JobDetailResponse : ResourceResponseHetoas
 {
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
     public decimal Salary { get; set; }
     public ICollection<string> Requirements { get; set; } = new List<string>();
+    // public ICollection<LinkResponseHetoas> Links { get; set; } = new List<LinkResponseHetoas>();
 }
