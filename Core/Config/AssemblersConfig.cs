9f52ded165e8f9292ead08cf72bc8d2f5fa51a84
Otthon Leão <otthonleao@hotmail.com>
2025-01-03 05:10:49 -0400

    chore: add Assemblers to JobDetailResponse with Hetoas URI dynamic format

--- Core/Config/AssemblersConfig.cs
+++ Core/Config/AssemblersConfig.cs
@@ -9,5 +9,6 @@ public static class AssemblersConfig
     public static void RegisterAssemblers(this IServiceCollection services)
     {
         services.AddScoped<IAssemblerHetoas<JobSummaryResponse>, JobSummaryAssemblerHetoas>();
+        services.AddScoped<IAssemblerHetoas<JobDetailResponse>, JobDetailAssemblerHetoas>();
     }
 }
