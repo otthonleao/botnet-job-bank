a0e0ae6faf55aa05e9d3aeb67635cc40b588beae
Otthon Leão <otthonleao@hotmail.com>
2025-01-02 22:06:17 -0400

    refactor: change DatabaseConfig and compose file

--- Core/Data/Context/JobBankDbContext.cs
+++ Core/Data/Context/JobBankDbContext.cs
@@ -4,9 +4,14 @@ using Microsoft.EntityFrameworkCore;
 
 namespace JobBank.Core.Data.Context;
 
-public class JobBankDbContext(DbContextOptions<JobBankDbContext> options) : DbContext(options)
+public class JobBankDbContext : DbContext
 {
+    public JobBankDbContext(DbContextOptions<JobBankDbContext> options) : base(options)
+    {
+    }
+
     public DbSet<Job> Jobs => Set<Job>();
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.ApplyConfiguration(new JobEntityConfig());
